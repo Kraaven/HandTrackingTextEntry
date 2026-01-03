180b27c5fe4580507a7dee0e2990485d75c3fa6f
Kraaven <dhruv.bhojaraj@gmail.com>
2026-01-03 19:10:06 +0530

    Creatred a basic custom hand interaction system

--- Assets/Src/Main/Scripts/FingerTipCollider.cs
+++ Assets/Src/Main/Scripts/FingerTipCollider.cs
@@ -3,5 +3,14 @@ using UnityEngine.Events;
 
 public class FingerTipCollider : MonoBehaviour
 {
+    [Header("Events")]
+    public UnityEvent<Collision> OnFingerTipCollide;
+    public UnityEvent<Collision> OnFingerTipExit;
+    [Header("Collider Type")]
+    public HandType HandType;
+    public FingerType FingerType;
 
 }
+
+public enum HandType { Left, Right}
+public enum FingerType { Thumb, Index, Middle, Ring, Little};
